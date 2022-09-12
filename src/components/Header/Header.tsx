c18a5325538fee4e0a2e44f8f3be4ee8065094c3
GlebS1mple <gleb.gonchar.77@mail.ru>
2022-09-12 21:42:44 +0300

    adding header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import s from './Header.module.css'
+import logo from '../../../src/img/Logo.png'
+import search from '../../..//src/img/search.png'
 
 const Header = () => {
     return (
-        <div>
-            hi
+        <div className={s.main}>
+            <div className={s.container}>
+                <div className={s.logoBlock}>
+                    <img src={logo} alt="Logo" className={s.logo} />
+                    <div className={s.name}>Rest</div>
+                </div>
+                <div className={s.search}>
+                    <form className={s.form}>
+                        <div className={s.inputs}>
+                            <input type="text" placeholder='Restaurants' className={`${s.searchInput} ${s.serchInputRestaurants}`} />
+                            <input type="text" placeholder='New York' className={s.searchInput} />
+                        </div>
+                        <button className={s.searchButton}>
+                            <img src={search} alt="Search" className={s.searchImg} />
+                        </button>
+                    </form>
+
+                </div>
+                <button className={s.link}>For business</button>
+                <button className={s.link}>Write some review</button>
+            </div>
         </div>
     );
 };
