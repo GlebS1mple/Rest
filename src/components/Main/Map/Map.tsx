6730c37d534fb45f928a4812fa056234caf11d2b
GlebS1mple <gleb.gonchar.77@mail.ru>
2022-10-15 12:08:56 +0300

    add markers on map component

--- src/components/Main/Map/Map.tsx
+++ src/components/Main/Map/Map.tsx
@@ -1,9 +1,10 @@
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-import React, { ReactElement, useRef, useEffect } from 'react';
+import React, { ReactElement, useRef, useEffect, useState } from 'react';
 import s from './Map.module.css'
 import { useSelector } from 'react-redux';
 import { AppStateType } from "../../../redux/store";
+import { RestaurantsType } from "../../../types/types";
 
 const render = (status: Status): ReactElement => {
     if (status === Status.FAILURE) return <div>Error...</div>;
@@ -19,23 +20,37 @@ function MyMapComponent({
     zoom: number;
     children: any
 }) {
+    const trueCenter = useSelector<AppStateType, { lat: number, lng: number }>(state => state.main.mapCenter)
     const ref = useRef();
+    const [map, setMap] = React.useState<google.maps.Map>();
+    console.log('render')
     useEffect(() => {
         //@ts-ignore
-        new window.google.maps.Map(ref.current, {
+        setMap(new window.google.maps.Map(ref.current, {
             center,
             zoom,
-        });
-    }, [ref]);
+        }))
+    }, [ref, trueCenter]);
+    return (
+        <>
+            {//@ts-ignore
+                <div ref={ref} id="map" className={s.main} />
+            }
+            {React.Children.map(children, (child) => {
+                if (React.isValidElement(child)) {
+                    //@ts-ignore
+                    return React.cloneElement(child, { map });
+                }
+            })}
+        </>
+    );
     //@ts-ignore
-    return <div ref={ref} id="map" className={s.main} />;
 }
 const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
     const [marker, setMarker] = React.useState<google.maps.Marker>();
-
     React.useEffect(() => {
         if (!marker) {
-            setMarker(new google.maps.Marker());
+            setMarker(new google.maps.Marker({ position: { lat: 40.736218, lng: -73.99597 } }));
         }
 
         // remove marker from map on unmount
@@ -55,16 +70,19 @@ const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
     return null;
 };
 function Map() {
+    const restaurants = useSelector<AppStateType, Array<RestaurantsType>>(state => state.main.restaurants)
+    const filteredRestaurants = useSelector<AppStateType, Array<RestaurantsType>>(state => state.main.filteredRestaurants)
     const center = useSelector<AppStateType, { lat: number, lng: number }>(state => state.main.mapCenter)
-    const zoom = 10;
-
+    const zoom = 13;
     return (
         //@ts-ignore
         <Wrapper className={s.wrapper} apiKey={process.env.REACT_APP_API_GOOGLE_KEY} render={render}>
             <MyMapComponent center={center} zoom={zoom}>
-                <Marker position={center} />
+                {//@ts-ignore
+                    filteredRestaurants.length > 0 ? restaurants.map(rest => <Marker key={rest.id} position={Object.values(rest.coordinates).reduce(function (prev, curr) { return { lat: prev, lng: curr } })} />) :
+                        //@ts-ignore
+                        restaurants.length > 0 ? restaurants.map(rest => <Marker key={rest.id} position={Object.values(rest.coordinates).reduce(function (prev, curr) { return { lat: prev, lng: curr } })} />) : ''}
             </MyMapComponent>
-
         </Wrapper>
     );
 }
