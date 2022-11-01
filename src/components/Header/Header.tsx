80294871b0a0ed7e445a1a13fbc5fdd9bd89102f
GlebS1mple <gleb.gonchar.77@mail.ru>
2022-11-01 19:48:02 +0300

    add styles for mobile for header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import { actions } from './../../redux/mainReducer'
 import { useNavigate, useLocation } from 'react-router-dom'
 
 
-const Header: React.FC = () => {
+const Header: React.FC = React.memo(() => {
     let navigate = useNavigate()
     let loc = useLocation();
     const [location, setLocation] = useState<string>('NewYork')
@@ -25,26 +25,30 @@ const Header: React.FC = () => {
         <div className={s.main}>
             <div className={s.container}>
                 <div className={s.logoBlock}>
-                    <img src={logo} alt="Logo" className={s.logo} />
-                    <div className={s.name}>Rest</div>
+                    <div className={s.log}>
+                        <img src={logo} alt="Logo" className={s.logo} />
+                        <div className={s.name}>Rest</div>
+                    </div>
+                    <button onClick={search} className={s.searchMobileButton}>
+                        Search
+                    </button>
                 </div>
-                <div className={s.search}>
-                    <form className={s.form}>
-                        <div className={s.inputs}>
-                            <input onChange={(e) => setRestaurant(e.target.value)} type="text" placeholder='Restaurants' className={`${s.searchInput} ${s.serchInputRestaurants}`} />
-                            <input onChange={(e) => setLocation(e.target.value)} type="text" placeholder='New York' className={s.searchInput} />
-                        </div>
-                        <button onClick={search} className={s.searchButton}>
-                            <img src={searchPhoto} alt="Search" className={s.searchImg} />
-                        </button>
-                    </form>
-                </div>
+                <form className={s.form}>
+                    <div className={s.inputs}>
+                        <input onChange={(e) => setRestaurant(e.target.value)} type="text" placeholder='Restaurants' className={`${s.searchInput} ${s.serchInputRestaurants}`} />
+                        <input onChange={(e) => setLocation(e.target.value)} type="text" placeholder='New York' className={s.searchInput} />
+                    </div>
+                    <button onClick={search} className={s.searchButton}>
+                        <img src={searchPhoto} alt="Search" className={s.searchImg} />
+                    </button>
+                </form>
+
                 <button className={s.link}>For business</button>
                 <button className={s.link}>Write some review</button>
             </div>
         </div>
     );
-};
+});
 
 export default Header;
 
